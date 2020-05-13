9afc88f17cf36c8d2d5bc1bbb13c6d13a25a5bc7
egrwka <topsteamegor@list.ru>
2020-05-13 20:26:43 +0300

    first commit

---
+++ DragAndDrop/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace DragAndDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox1.Location = new System.Drawing.Point(1, -2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(749, 532);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(61, 430);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Вид";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Font = new System.Drawing.Font("Sitka Small", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(559, 430);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Информация";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Font = new System.Drawing.Font("Sitka Small", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(250, 430);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Форма";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Sitka Small", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(277, 287);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 43);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Наведи сюда";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 529);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Drag and Drop";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DragAndDrop/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragAndDrop
+{
+    public partial class Form1 : Form
+    {
+        Rectangle Rectangle = new Rectangle(10, 10, 200, 100);
+        Rectangle Cirkle = new Rectangle(220, 10, 150, 150);
+        Rectangle Square = new Rectangle(380, 10, 150, 150);
+        bool RectangleClicked, CircleClicked, SquareClicked = false;
+        int RectangleX, CircleX2, SquareX3, RectangleY, CircleY2, SquareY3 = 0;
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            RectangleClicked = false;
+            CircleClicked = false;
+            SquareClicked = false;
+            if (LastClicked == 2)
+            {
+                if ((label2.Location.X < Cirkle.X + Cirkle.Width) && (label2.Location.X > Cirkle.X))
+                {
+                    if ((label2.Location.Y < Cirkle.Y + Cirkle.Height) && (label2.Location.Y > Cirkle.Y))
+                    {
+                        X = Cirkle.X;
+                        Y = Cirkle.Y;
+                        dX = CircleX2;
+                        dY = CircleY2;
+                        Cirkle.X = Rectangle.X;
+                        Cirkle.Y = Rectangle.Y;
+                        CircleX2 = RectangleX;
+                        CircleY2 = RectangleY;
+
+                        Rectangle.X = X;
+                        Rectangle.Y = Y;
+                        RectangleX = dX;
+                        RectangleY = dY;
+                    }
+                }
+            }
+            if (LastClicked == 3)
+            {
+                if ((label2.Location.X < Square.X + Square.Width) && (label2.Location.X > Square.X))
+                {
+                    if ((label2.Location.Y < Square.Y + Square.Height) && (label2.Location.Y > Square.Y))
+                    {
+                        X = Square.X;
+                        Y = Square.Y;
+                        dX = SquareX3;
+                        dY = SquareY3;
+                        Square.X = Cirkle.X;
+                        Square.Y = Cirkle.Y;
+                        SquareX3 = CircleX2;
+                        SquareY3 = CircleY2;
+
+                        Cirkle.X = X;
+                        Cirkle.Y = Y;
+                        CircleX2 = dX;
+                        CircleY2 = dY;
+                    }
+                }
+            }
+            if (LastClicked == 1)
+            {
+                if ((label2.Location.X < Rectangle.X + Rectangle.Width) && (label2.Location.X > Rectangle.X))
+                {
+                    if ((label2.Location.Y < Rectangle.Y + Rectangle.Height) && (label2.Location.Y > Rectangle.Y))
+                    {
+                        X = Rectangle.X;
+                        Y = Rectangle.Y;
+                        dX = RectangleX;
+                        dY = RectangleY;
+                        Rectangle.X = Square.X;
+                        Rectangle.Y = Square.Y;
+                        RectangleX = SquareX3;
+                        RectangleY = SquareY3;
+
+                        Square.X = X;
+                        Square.Y = Y;
+                        SquareX3 = dX;
+                        SquareY3 = dY;
+                    }
+                }
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.X < Rectangle.X + Rectangle.Width) && (e.X > Rectangle.X))
+            {
+                if ((e.Y < Rectangle.Y + Rectangle.Height) && (e.Y > Rectangle.Y))
+                {
+                    RectangleClicked = true;
+
+                    RectangleX = e.X - Rectangle.X;
+                    RectangleY = e.Y - Rectangle.Y;
+                    //LastClicked = 1;
+                }
+            }
+            if ((e.X < Cirkle.X + Cirkle.Width) && (e.X > Cirkle.X))
+            {
+                if ((e.Y < Cirkle.Y + Cirkle.Height) && (e.Y > Cirkle.Y))
+                {
+                    CircleClicked = true;
+
+                    CircleX2 = e.X - Cirkle.X;
+                    CircleY2 = e.Y - Cirkle.Y;
+                    //LastClicked = 2;
+                }
+            }
+            if ((e.X < Square.X + Square.Width) && (e.X > Square.X))
+            {
+                if ((e.Y < Square.Y + Square.Height) && (e.Y > Square.Y))
+                {
+                    SquareClicked = true;
+
+                    SquareX3 = e.X - Square.X;
+                    SquareY3 = e.Y - Square.Y;
+                    //LastClicked = 3;
+                }
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(Brushes.Red, Cirkle);
+            e.Graphics.FillRectangle(Brushes.Blue, Square);
+            e.Graphics.FillRectangle(Brushes.Yellow, Rectangle);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (CircleClicked)
+            {
+                Cirkle.X = e.X - CircleX2;
+                Cirkle.Y = e.Y - CircleY2;
+                pictureBox1.Invalidate();
+            }
+            if (RectangleClicked)
+            {
+                Rectangle.X = e.X - RectangleX;
+                Rectangle.Y = e.Y - RectangleY;
+                pictureBox1.Invalidate();
+            }
+            if (SquareClicked)
+            {
+                Square.X = e.X - SquareX3;
+                Square.Y = e.Y - SquareY3;
+                pictureBox1.Invalidate();
+            }
+            if ((label4.Location.X < Square.X + Square.Width) && (label4.Location.X > Square.X))
+            {
+                if ((label4.Location.Y < Square.Y + Square.Height) && (label4.Location.Y > Square.Y))
+                {
+                    label1.Text = "Синий";
+                    label2.Text = "Квадрат";
+                    label3.Text = "Синий квадрат";
+                }
+            }
+            else if ((label4.Location.X < Cirkle.X + Cirkle.Width) && (label4.Location.X > Cirkle.X))
+            {
+                if ((label4.Location.Y < Cirkle.Y + Cirkle.Height) && (label4.Location.Y > Cirkle.Y))
+                {
+                    label1.Text = "Красный";
+                    label2.Text = "Круг";
+                    label3.Text = "Красный круг";
+                }
+            }
+            else if ((label4.Location.X < Rectangle.X + Rectangle.Width) && (label4.Location.X > Rectangle.X))
+            {
+                if ((label4.Location.Y < Rectangle.Y + Rectangle.Height) && (label4.Location.Y > Rectangle.Y))
+                {
+                    label1.Text = "Жёлтый";
+                    label2.Text = "Прямоугольник";
+                    label3.Text = "Жёлтый прямоугольник";
+                }
+            }
+            else
+            {
+                label1.Text = "Вид";
+                label2.Text = "Форма";
+                label3.Text = "Информация";
+            }
+        }
+
+        int X, Y, dX, dY;
+        int LastClicked = 0;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
